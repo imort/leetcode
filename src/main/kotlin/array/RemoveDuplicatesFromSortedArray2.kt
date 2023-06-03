e787185c873ddd0e43de8fe73865f204f7183bc4
imort <imort@joom.com>
2023-06-03 14:54:02 +0100

    RemoveDuplicatesFromSortedArray2

---
+++ src/main/kotlin/array/RemoveDuplicatesFromSortedArray2.kt
@@ -0,0 +1,36 @@
+package array
+
+/**
+ * Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place
+ * such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+ *
+ * Since it is impossible to change the length of the array in some languages,
+ * you must instead have the result be placed in the first part of the array nums.
+ * More formally, if there are k elements after removing the duplicates,
+ * then the first k elements of nums should hold the final result.
+ * It does not matter what you leave beyond the first k elements.
+ *
+ * Return k after placing the final result in the first k slots of nums.
+ *
+ * Do not allocate extra space for another array.
+ * You must do this by modifying the input array in-place with O(1) extra memory.
+ */
+class RemoveDuplicatesFromSortedArray2 {
+    fun removeDuplicates(nums: IntArray): Int {
+        var count = 0
+        var prev: Int? = null
+        var prevCount = 0
+        for (i in nums.indices) {
+            val num = nums[i]
+            if (num == prev) {
+                if (prevCount == 2) continue
+                prevCount++
+            } else {
+                prev = num
+                prevCount = 1
+            }
+            nums[count++] = num
+        }
+        return count
+    }
+}
